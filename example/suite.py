15bbaefab6fbd9524e421e161414673386906dd6
Hanno Sternberg <drakehutner@gmail.com>
2012-10-19 20:08:54 +0200

    Added lambda expression example

--- example/suite.py
+++ example/suite.py
@@ -18,5 +18,16 @@ test3 = {
 	"name":"Example 3",
 	"descr":"This test will produce an error"
 }
-
-suite = [test1, test2, test3]
+test4 = {
+	"name":"Example 4",
+	"descr":"Lambda expression success",
+	"cmd":"dut.py Some more text",
+	"expect":lambda x: x.find("text") > 0 
+}
+test5 = {
+	"name":"Example 5",
+	"descr":"Lambda expression fail",
+	"cmd":"dut.py Some more text",
+	"expect":lambda x: x.find("test") > 0 
+}
+suite = [test1, test2, test3, test4, test5]
