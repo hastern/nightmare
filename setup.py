5e3ad345c24d7e1626c8cde0d39cc4b42d14838f
Hanno Sternberg <drakehutner@gmail.com>
2013-12-02 21:56:32 +0100

    Updated the setup.py to get a better look on windows, added the manifest

--- setup.py
+++ setup.py
@@ -11,11 +11,71 @@ except:
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
     
+Name = "nightmare"
+Description = "NIGHTMARE is of Generous Help when Testing; May Arnold be Remembered Eternally"
+Version = "2.0"
+Author = "Hanno Sternberg"
+Mail = "hanno@almostintelligent.de"
+Url = 'https://github.com/drakehutner/pyTest'
+Company = ''
+Copyright = ''
+
+    
+
+MANIFEST_TEMPLATE = """
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+  <assemblyIdentity 
+    version="5.0.0.0"
+    processorArchitecture="x86"
+    name="%(prog)s"
+    type="win32"
+  />
+  <description>%(descr)s</description>
+  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+      <requestedPrivileges>
+        <requestedExecutionLevel level="asInvoker" uiAccess="false">
+        </requestedExecutionLevel>
+      </requestedPrivileges>
+    </security>
+  </trustInfo>
+  <dependency>
+    <dependentAssembly>
+      <assemblyIdentity
+            type="win32"
+            name="Microsoft.VC90.CRT"
+            version="9.0.21022.8"
+            processorArchitecture="x86"
+            publicKeyToken="1fc8b3b9a1e18e3b">
+      </assemblyIdentity>
+    </dependentAssembly>
+  </dependency>
+  <dependency>
+    <dependentAssembly>
+         <assemblyIdentity
+            type="win32"
+            name="Microsoft.Windows.Common-Controls"
+            version="6.0.0.0"
+            processorArchitecture="X86"
+            publicKeyToken="6595b64144ccf1df"
+            language="*"
+        />
+    </dependentAssembly>
+  </dependency>
+</assembly>
+"""    
+    
 includes = ['pyTest','pyTestMain','pyTestSuite','pyTestRunner','pyTestGui','pyTestEditForm','pyTestUtils','__main__','arnold_converter']
 excludes = ['pyreadline','pyreadline.console', 'pyreadline.rlmain','unittest','email', 'email.Utils','calendar','_ssl','Tkinter',"Tkconstants", "tcl"]
 packages = ['pyparsing','colorama']
+icon_resources = []
+bitmap_resources = []
+other_resources = []
+other_resources = [(24, 1, MANIFEST_TEMPLATE % dict(prog=Name, descr=Description))]
 dll_excludes = ['w9xpopen.exe',"MSVCP90.dll"]
 mainScript = 'pyTestMain'
+
 
 if py2exe is not None:
 	options = {"py2exe": {
@@ -33,15 +93,22 @@ if py2exe is not None:
 	}
 else:
 	options = {}
+	
+GUI2Exe_Target = {
+	"script": mainScript + ".py",
+	"icon_resources": icon_resources,
+	"bitmap_resources": bitmap_resources,
+	"other_resources": other_resources,
+	"dest_base": Name,
+	"version": Version,
+	"company_name": Company,
+	"copyright": Copyright,
+	"name": Name
+}
 
 setup(
-	name='nightmare',
-	#name='pyTest',
-	version='2.0',
-	description='NIGHTMARE is of Generous Help when Testing; May Arnold be Remembered Eternally',
-	author='Hanno Sternberg',
-	author_email='hanno@almostintelligent.de',
-	url='https://github.com/drakehutner/pyTest',
+	name=Name, version=Version, description=Description,
+	author=Author, author_email=Mail, url=Url,
 	py_modules=includes,
 	license=read('LICENSE'),
 	long_description=read('README.md'),
@@ -52,7 +119,8 @@ setup(
 	},
 	install_requires=['pyparsing'],
 	options = options,
-	console=[mainScript+'.py'],
+	#console=[mainScript+'.py'],
+	console=[GUI2Exe_Target],
 	data_files=[('',['example/suite.py'])],
 	#scripts=['pyTest.py'],
 	zipfile=None,
