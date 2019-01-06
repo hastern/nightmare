fd818ffb8e96fca5e6e8c29eea35dae2c1d5367b
Hanno Sternberg <hanno@almostintelligent.de>
2019-01-06 23:37:24 +0100

    Auto generate __main__.py for egg-file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import os
-from setuptools import setup
+import setuptools
 
 try:
     import py2exe
@@ -9,18 +9,24 @@ except:
     py2exe = None
 
 import nightmare
+import pathlib
 
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+# Create a __main__.py to be included in the egg
+main_py = """#!/usr/bin/env python3
 
-packages = ["pyparsing", "nightmare"]
-icon_resources = [(1, "resource/nightmare.ico")]
-mainScript = "__main__"
+import nightmare.__main__
 
+if __name__ == "__main__":
+    nightmare.__main__.main()
+"""
+main = pathlib.Path("__main__.py")
+main.write_text(main_py)
 
-setup(
+setuptools.setup(
     name="nightmare",
     version=nightmare.__version__,
     description="NIGHTMARE is of Generous Help when Testing; May Arnold be Remembered Eternally",
@@ -30,9 +36,19 @@ setup(
     license=read("LICENSE"),
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    py_modules=["__main__"],
     install_requires=["pyparsing"],
+    entry_points={
+        'setuptools.installation': [
+            'eggsecutable = nightmare.__main__:main',
+        ]
+    },
     extras_require = {
         'gui': ["wxpython"]
     },
     data_files=[("", ["resource/nightmare.ico"])],
 )
+
+# And delete __main__.py (We don't want this to pollute or directory)
+main.unlink()
